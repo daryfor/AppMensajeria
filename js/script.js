01cefec4a9d3ef40722fa462bf103e7a07d59f96
Dario Diez <darioeschuky@yahoo.es>
2021-10-05 23:37:09 +0200

    Comenzado el Javascript y añadido un popup en el HTML para agregar nuevo canal

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function fct_callpopup() {
+    document.querySelector(".popup").style.display = "flex";
+}
+
+function fct_closeWindow() {
+    document.querySelector(".popup").style.display = "none";
+}
+
+function fct_createChannel() {
+    alert("New channel");
+
+
+}
+
+function fct_search() {
+    alert("search finished");
+}
+
+function fct_newMessage() {
+    alert("new message created");
+}
